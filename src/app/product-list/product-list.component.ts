19654ab42db88f3b714fed3e319d6234c5777f80
Yume28 <steevensept09@gmail.com>
2023-03-29 16:24:15 +0800

    change module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -15,7 +15,7 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit() {
     this._productService.getProducts().subscribe(response =>
-      this.products = response);
+    this.products = response);
   }
 
 }
